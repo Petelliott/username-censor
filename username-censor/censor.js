2a2c8cf27027a074ca1b7aefef11816ae3fc3dd8
Peter Elliott <p.elliott332@gmail.com>
2016-12-21 22:38:32 -0700

    refactord censor.js

--- username-censor/censor.js
+++ username-censor/censor.js
@@ -4,20 +4,24 @@ class_list = {"www.reddit.com":["author"]};
 chrome.runtime.onMessage.addListener(function(request, sender, callback) {
     var site = window.location.hostname;
 
-    if (request.action == "toggle_censor" && site in class_list) {
-        var usernames = [];
-        for (let i = 0; i < class_list[site].length; ++i) {
-            usernames = usernames.concat(document.getElementsByClassName(class_list[site][i]));
-        }
-
-        for (let i = 0; i < usernames.length; ++i) {
-            usernames[i].style.color = "red";
-            usernames[i].style.backgroundColor = "red";
-        }
-
-    } else if (!(site in class_list)) {
+    if (!(site in class_list)) {
         console.error("unsupported site");
-    } else {
-        console.error("unrecognized action");
+        return;
     }
+    if (request.action != "toggle_censor"){
+        console.error("unrecognized action");
+        return;
+    }
+
+
+    var usernames = [];
+    for (let i = 0; i < class_list[site].length; ++i) {
+        usernames = usernames.concat(document.getElementsByClassName(class_list[site][i]));
+    }
+
+    for (let i = 0; i < usernames.length; ++i) {
+        usernames[i].style.color = "red";
+        usernames[i].style.backgroundColor = "red";
+    }
+
 });
