0159e584656d5a6eb0801ed0d7d3128391690eac
Peter Elliott <p.elliott332@gmail.com>
2016-12-22 10:53:36 -0700

    gave up on tumblr for now

--- username-censor/censor.js
+++ username-censor/censor.js
@@ -2,7 +2,25 @@
 get_users = {
     "www.reddit.com": function() {
         return document.getElementsByClassName("author");
-    }
+    },
+    // "staff.tumblr.com": function() { TODO: add tumblr support later
+    //     const classes = ["asker","answerer","tumblr_blog"];
+    //
+    //     var users = [];
+    //     for (let i = 0; i < classes.length; ++i) {
+    //         users = users.concat(Array.prototype.slice.call(
+    //                 document.getElementsByClassName(classes[i])
+    //         ));
+    //     }
+    //
+    //     for (let i = 0; i < users.length; ++i) {
+    //         if (users[i].tagName != "a") {
+    //             users.splice(i, 1);
+    //         }
+    //     }
+    //     console.log(users);
+    //     return users;
+    // }
 };
 
 chrome.runtime.onMessage.addListener(function(request, sender, callback) {
